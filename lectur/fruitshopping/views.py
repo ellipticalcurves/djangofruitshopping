6c9607dc3db135a51bc99c1b42aac1b16c8763f4
Amin Akhtar <amin.akhtar19@imperial.ac.uk>
2023-06-23 00:17:29 +0100

    did some stuff

--- lectur/fruitshopping/views.py
+++ lectur/fruitshopping/views.py
@@ -4,3 +4,10 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, "fruitshopping/index.html")
+
+def greet(request, name):
+    return render(request, "fruitshopping/base.html",
+                  {
+                      "name":name.capitalize()
+                      }
+                      )
