d4c5f6b7e479c5fbf601de5782157cb76268fe4b
Amin Akhtar <amin.akhtar19@imperial.ac.uk>
2023-07-01 17:18:47 +0100

    add sessions

--- lectur/fruitshopping/urls.py
+++ lectur/fruitshopping/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-
 from . import views
 
+
+app_name = "shop"
 urlpatterns = [
      path("", views.index, name="index"),
-     path("<str:name>", views.greet, name="greet")
+    #  path("<str:name>", views.greet, name="greet"),
      path("add", views.add, name="add")
  ]
--- lectur/fruitshopping/views.py
+++ lectur/fruitshopping/views.py
@@ -1,12 +1,20 @@
+from django import forms
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 
+items = []#"apple", "banana", "watermelon"]
 
-items = ["apple", "banana", "watermelon"]
-
+class NewItemForm(forms.Form):
+    item = forms.CharField(label="New Item")
+    # price = forms.IntegerField(label="price",
+    #                             min_value=1,
+    #                             max_value=5 )
 # Create your views here.
 def index(request):
-
+    if "items" not in request.session:
+        request.session["items"] = []
     return render(request,
                    "fruitshopping/index.html",
                    {"items":items} # variable name in htnl template | value
@@ -14,12 +22,30 @@ def index(request):
 
 
 def add(request):
-    return render(request, "fruitshopping/add.html")
+    if request.method=="POST":
+        form = NewItemForm(request.POST)
+        if form.is_valid():
+            item = form.cleaned_data["item"]
+            items.append(item)
+            return HttpResponseRedirect(reverse("shop:index"))
+        else:
+            return render(request,
+                        "fruitshopping/add.html",
+                        {"form":form}
+                        )
 
-def greet(request, name):
     return render(request,
-                   "fruitshopping/base.html",
-                  {
-                      "name":name.capitalize()
-                      }
-                      )
+                   "fruitshopping/add.html",
+                   {
+                       "form":NewItemForm()
+                   }
+                   
+                   )
+
+# def greet(request, name):
+#     return render(request,
+#                    "fruitshopping/base.html",
+#                   {
+#                       "name":name.capitalize()
+#                       }
+#                       )
