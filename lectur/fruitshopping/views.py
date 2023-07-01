c4c08673be1c552359cead48b932ad033054cc4f
Amin Akhtar <amin.akhtar19@imperial.ac.uk>
2023-07-01 13:28:15 +0100

    added list functionality

--- lectur/fruitshopping/views.py
+++ lectur/fruitshopping/views.py
@@ -1,12 +1,24 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+
+items = ["apple", "banana", "watermelon"]
+
 # Create your views here.
 def index(request):
-    return render(request, "fruitshopping/index.html")
+
+    return render(request,
+                   "fruitshopping/index.html",
+                   {"items":items} # variable name in htnl template | value
+                   ) 
+
+
+def add(request):
+    return render(request, "fruitshopping/add.html")
 
 def greet(request, name):
-    return render(request, "fruitshopping/base.html",
+    return render(request,
+                   "fruitshopping/base.html",
                   {
                       "name":name.capitalize()
                       }
